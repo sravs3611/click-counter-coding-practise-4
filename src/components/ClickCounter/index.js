c888f21a6f98b0b9be2dc9b8b55e754c361804d2
sravan <mekalasravankumar66@gmail.com>
2024-06-18 12:40:53 +0000

    find bug

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import Components from 'react'
+
+import './index.css'
+
+class clickCounter extends Components {
+  render() {
+    return(
+    <div className="countainer">
+      <h1>The Button Has been Clicked Times</h1>
+    </div>
+    )
+  }
+}
+
+export default clickCounter
